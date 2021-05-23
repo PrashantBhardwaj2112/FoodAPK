0155744aa112ffed4a0664eaf25697eca9524d98
PrashantBhardwaj2112 <PrashantBhardwaj2112@gmil.com>
2021-05-23 20:43:01 +0530

    FoodAPK

---
+++ src/components/Products/data.js
@@ -0,0 +1,66 @@
+import burger1 from '../../images/burger1.jpg';
+import burger2 from '../../images/burger2.jpg';
+import burger3 from '../../images/burger3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: burger1,
+    alt: 'Burger',
+    name: 'Supreme BURGER',
+    desc:
+      'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '₹ 299',
+    button: 'Add to Cart'
+  },
+  {
+    img: burger2,
+    alt: 'Burger',
+    name: 'Hawaiian BURGER',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '₹ 344',
+    button: 'Add to Cart'
+  },
+  {
+    img: burger3,
+    alt: 'Burger',
+    name: 'Veggie Overload',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '₹ 454',
+    button: 'Add to Cart'
+  }
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '₹ 299',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '₹134',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '₹ 199',
+    button: 'Add to Cart'
+  }
+];
